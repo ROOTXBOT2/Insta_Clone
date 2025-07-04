df361b2207fe8df43fd065e0600e1778bcc3c284
ROOTXBOT2 <rootxbot2@gmail.com>
2025-07-04 17:57:03 +0900

    feat:feat: 프로젝트 Spring 기본 스켈레톤 생성

--- api/src/main/java/com/insta_clone/api/InstaCloneApiApplication.java
+++ api/src/main/java/com/insta_clone/api/InstaCloneApiApplication.java
@@ -1,8 +1,16 @@
 package com.insta_clone.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Insta_Clone API 명세서",
+				version = "v1.0.0",
+				description = "Insta_Clone 인스타그램 클론 백엔드 API 명세"
+		)
+)
 @SpringBootApplication
 public class InstaCloneApiApplication {
 
